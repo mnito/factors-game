9c3f21ef64904a42aa9626ef75ed8e3122258788
Michael P. Nitowski <mpnitowski@gmail.com>
2016-10-26 20:12:32 -0400

    Create Block.js

---
+++ Block.js
@@ -0,0 +1,20 @@
+function Block(value, position, size) {
+   this.value = value;
+   this.position = position;
+   this.value = value;
+}
+
+Block.prototype.setPosition = function(position) {
+   this.position = position;
+}
+
+Block.prototype.transform = function( block, goal ) {
+   var newValue = block.value;
+   if( goal.current === goal.divide ) {
+      newValue = block.value % this.value === 0 ? block.value / this.value : block.value * this.value;
+   } else {
+      //need to adjust this
+      newValue = this.value % block.value === 0 ? block.value * this.value : this.value / block.value;
+   }
+   block.value = newValue;
+}
