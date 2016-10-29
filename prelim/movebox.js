34ec9cdba1b70624289d5e92373317b96c52430a
Michael P. Nitowski <mpnitowski@gmail.com>
2016-10-29 11:52:14 -0400

    Rendering of other boxes and collision detection

--- prelim/movebox.js
+++ prelim/movebox.js
@@ -1,7 +1,8 @@
 /*
- * Move box across screen
+ * Move box across screen and basic collision detection
+ * Also fps addition
  * 
- * Still need to prevent jumping and work out touch stuff
+ * Still need to (maybe) prevent jumping and work out touch stuff
  */
 
 console.log('start');
@@ -16,7 +17,7 @@ canvas.id = 'view';
 
 document.body.appendChild(canvas);
 
-var x = 0;
+var x = (canvas.width - canvas.width * .15) / 2;
 
 //Need to figure out touch stuff, Firefox not firing touch events
 canvas.addEventListener('touchstart', function(e){
@@ -33,6 +34,7 @@ var isMouseDown = false;
 
 canvas.addEventListener('mousedown', function(e){
     isMouseDown = true;
+    x = e.clientX;
     e.preventDefault();
 }, false);
 
@@ -50,18 +52,44 @@ canvas.addEventListener('mouseup', function(e){
 
 brush = canvas.getContext('2d');
 
+var fps = 60;
+var timeout = 1000/fps;
+
 //Loop stuff
-function main(timestamp)
-{
-     window.requestAnimationFrame(main);
-     frame(timestamp);
+function main(timestamp) {
+    setTimeout(function(){        
+       window.requestAnimationFrame(main);
+       frame(timestamp);
+    }, timeout);
 }
 
-function frame(timestamp)
-{
-     brush.clearRect(0, 0, canvas.width, canvas.height);
-     var s = canvas.width * .15;
-     brush.fillStyle = 'red';
+var boxes =  [{x: 0, y: canvas.height - 100, s: canvas.height * .20}, {x: 300, y: canvas.height - 300, s: canvas.height * .30}];
+
+function frame(timestamp) {
+    brush.clearRect(0, 0, canvas.width, canvas.height);
+    var collide = false;
+    for each(otherBox in boxes) {
+     
+       brush.fillStyle = 'lime';
+       brush.fillRect(otherBox.x, otherBox.y, otherBox.s, otherBox.s);
+       otherBox.y -= .2;
+       if(otherBox.y + otherBox.s < 0) {
+           otherBox.y = canvas.height;
+       }
+       var s = canvas.width * .15;
+    
+       if (x < otherBox.x + otherBox.s &&
+           x + s > otherBox.x &&
+           canvas.height * .10 < otherBox.y + otherBox.s &&
+           s + canvas.height * .10 > otherBox.y) {
+          collide = true;
+       }
+     }
+     if(collide) {
+         brush.fillStyle = 'blue';
+     } else {
+         brush.fillStyle = 'red';
+     }
      brush.fillRect(x , canvas.height * .10, s, s);
 }
 
