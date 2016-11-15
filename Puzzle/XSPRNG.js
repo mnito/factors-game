7ac4ec6c8ca694450c44f7e00a252a26e203afa7
mnito <mpnitowski@gmail.com>
2016-11-14 20:29:19 -0500

    Implement seedable random number generator

---
+++ Puzzle/XSPRNG.js
@@ -0,0 +1,19 @@
+function XSPRNG(seed) {
+    this.seed = seed;
+    this.previous = seed;
+    //Max signed 32-bit integer + 1
+    this.max = 0x7FFFFFFF + 1;
+}
+
+XSPRNG.prototype.random = function() {
+    var next = XSPRNG.xorshift32( this.previous );
+    this.previous = next;
+    return next / this.max;
+}
+
+XSPRNG.xorshift32 = function(number) {
+    number ^= number << 13;
+    number ^= number << 5;
+    number ^= number >> 17;
+    return number >>> 0;
+}
