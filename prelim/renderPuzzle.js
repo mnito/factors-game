b61f43e7e64e14f2475b86d9c5481e3bbae74d59
mnito <mpnitowski@gmail.com>
2016-12-03 22:31:50 -0500

    Include basic puzzle rendering function

---
+++ prelim/renderPuzzle.js
@@ -0,0 +1,22 @@
+function renderPuzzle( levelNumber, canvas, width, height ) {
+    
+    document.body.appendChild(canvas);
+
+    canvas.width = width;
+    canvas.height = height;
+
+    canvas.style.backgroundColor = '#333333';
+
+    canvas.id = 'gameView';
+
+    var levels = new Levels( new XSPRNG(1), 70, new MonochromaticPaletteBuilder());
+    var level = levels.get(levelNumber);
+
+    var vc = new ViewController( level, canvas, 2, 'white' );
+
+    vc.draw();
+
+    var inputController = new InputController( vc );
+    var keyboardInput = new KeyboardInput(inputController);
+    keyboardInput.listen();
+}
