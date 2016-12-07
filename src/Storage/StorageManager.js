f14af07c76e111a4b8beb66b4b07869cfe6673db
mnito <mpnitowski@gmail.com>
2016-12-06 22:42:48 -0500

    Storage manager implementation

---
+++ src/Storage/StorageManager.js
@@ -0,0 +1,37 @@
+function StorageManager(storageMethod) {
+    this.storageMethod = storageMethod;
+}
+
+StorageManager.prototype.setCurrentLevel = function(level) {
+    this.storageMethod.setItem('currentLevel', level);
+};
+
+StorageManager.prototype.getCurrentLevel = function() {
+    return this.storageMethod.getItem('currentLevel');
+};
+
+StorageManager.prototype.setCurrentLevelHistory = function(history) {
+    this.history = history;
+};
+
+StorageManager.prototype.updateCurrentLevelHistory = function() {
+    if(typeof this.history === 'undefined' ) {
+        throw "Cannot update history without history object";
+    }
+    this.storageMethod.setItem('currentLevelHistory', JSON.stringify(this.history));
+};
+
+StorageManager.prototype.getLevelResults = function() {
+    var results = this.storageMethod.getItem('levelResults');
+    return results ? JSON.parse(results) : {};
+};
+
+StorageManager.prototype.setLevelResult = function(level, result) {
+    var results = this.getLevelResults();
+    results[level] = result;
+    this.storageMethod.setItem('levelResults', JSON.stringify(results));
+};
+
+StorageManager.prototype.getLevelResult = function(level) {
+    return this.getLevelResults()[level];
+};
