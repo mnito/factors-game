bfbfca57b19a4728523af4aec81a2f4753be3c14
mnito <mpnitowski@gmail.com>
2016-12-06 21:33:46 -0500

    Touch input control

---
+++ src/Input/TouchInput.js
@@ -0,0 +1,36 @@
+function TouchInput(inputController) {
+    this.inputController = inputController;
+}
+
+TouchInput.prototype.listen = function() {
+    document.addEventListener('touchstart', this.startTouch.bind(this));
+    document.addEventListener('touchmove', this.detectSwipe.bind(this));
+};
+
+TouchInput.prototype.startTouch = function(event) {
+    this.xStart = event.touches[0].clientX;
+    this.yStart = event.touches[0].clientY;
+};
+
+TouchInput.prototype.detectSwipe = function(event) {
+    if( this.xStart === null || this.yStart === null) {
+        return;
+    }
+
+    var xEnd = event.touches[0].clientX;
+    var yEnd = event.touches[0].clientY;
+
+    var deltaX = xEnd - this.xStart;
+    var deltaY = yEnd - this.yStart;
+
+    var isHorizontalSwipe = Math.abs(deltaX) > Math.abs(deltaY);
+
+    if(isHorizontalSwipe) {
+        deltaX < 0 ? this.inputController.left(): this.inputController.right();
+    } else if(deltaY > 0) {
+        this.inputController.down();
+    }
+
+    this.xStart = null;
+    this.yStart = null;
+};
