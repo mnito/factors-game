59ffef1638980502a918825dc713e41f090d3cba
mnito <mpnitowski@gmail.com>
2016-12-06 20:45:34 -0500

    Add vim key bindings to KeyboardInput

--- src/Input/KeyboardInput.js
+++ src/Input/KeyboardInput.js
@@ -8,14 +8,17 @@ KeyboardInput.prototype.listen = function() {
      switch(e.keyCode) {
        case 37 :
        case 65 :
+       case 72 :
          inputController.left();
          break;
        case 40 :
+       case 74 :
        case 83 :
          inputController.down();
          break;
        case 39 :
        case 68 :
+       case 76 :
          inputController.right();
          break;
      }
