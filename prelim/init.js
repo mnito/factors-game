8494d646a79771acd71b0d4c0fbdc0fab91dccc0
mnito <mpnitowski@gmail.com>
2016-12-08 22:19:35 -0500

    Preliminary init function

---
+++ prelim/init.js
@@ -0,0 +1,24 @@
+function init(canvas, width, height) {
+    document.body.appendChild(canvas);
+
+    canvas.width = width;
+    canvas.height = height;
+
+    canvas.style.backgroundColor = '#333333';
+
+    canvas.id = 'gameView';
+
+    var levels = new Levels( new XSPRNG(1), 70, new MonochromaticPaletteBuilder());
+    var level = levels.get(1);
+
+    var vc = new ViewController( level, canvas, 2, 'white' );
+
+    vc.draw();
+
+    var inputController = new InputController( vc );
+    var keyboardInput = new KeyboardInput(inputController);
+    var touchInput = new TouchInput(inputController, canvas);
+
+    keyboardInput.listen();
+    touchInput.listen();
+}
