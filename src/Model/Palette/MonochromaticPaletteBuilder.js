ca6430ad1eb562120319f22ff156f241a16bdaeb
mnito <mpnitowski@gmail.com>
2017-02-27 12:37:53 -0500

    Add get color method to palette builder

--- src/Model/Palette/MonochromaticPaletteBuilder.js
+++ src/Model/Palette/MonochromaticPaletteBuilder.js
@@ -3,16 +3,16 @@ function MonochromaticPaletteBuilder(hue, saturation) {
   this.saturation = saturation;
 }
 
-MonochromaticPaletteBuilder.prototype.build = function (count, range) {
+MonochromaticPaletteBuilder.prototype.build = function(count, range) {
   if (count < 1) {
     throw 'Count cannot be less than 1.';
   }
-  
+
   var maxRange = 100;
   if (range > 100) {
     range = 100;
   }
-  
+
   var min = (100 - range) / 2;
   var h = this.hue;
   var s = this.saturation;
@@ -22,6 +22,19 @@ MonochromaticPaletteBuilder.prototype.build = function (count, range) {
   for(var c = count - 1; c >= 0; c -= 1) {
      palette.push(new HSL(h, s, Math.floor(interval * c + min)));
   }
-  
+
   return palette;
 };
+
+MonochromaticPaletteBuilder.prototype.getColor = function(number, count, range)
+{
+    var maxRange = 100;
+    if (range > 100) {
+      range = 100;
+    }
+
+    var min = (100 - range) / 2;
+    var interval = range / (count - 1);
+
+    return new HSL(this.hue, this.saturation, Math.floor(interval * (count / 2) + min));
+}
