a6bc03b09d04df24b049fa9f3e5c257b82040078
mnito <mpnitowski@gmail.com>
2017-03-16 22:02:11 -0400

    Remove extra space from if statement

--- src/Model/Game/Levels.js
+++ src/Model/Game/Levels.js
@@ -5,24 +5,24 @@ function Levels(prng, paletteRange, paletteBuilder) {
 }
 
 Levels.prototype.get = function(level) {
-   if( typeof this.prng.seed === 'function') {
+   if(typeof this.prng.seed === 'function') {
        this.prng.seed(level);
    }
    var hue = Math.floor(this.prng.random() * 360);
    var saturation = Math.floor(this.prng.random() * 20) + 80;
-   
+
    this.paletteBuilder.hue = hue;
    this.paletteBuilder.saturation = saturation;
 
    var boardColors = this.paletteBuilder.build(16, this.paletteRange);
    var numberColor = HSL.complement(boardColors[Math.floor(this.prng.random() * 16)]);
-   
+
    var levelPalette = new LevelPalette(numberColor, boardColors);
 
    var board = Board.create(4, 4, this.prng);
    board.shuffle();
-   
+
    var puzzle = new Puzzle(level, board);
-   
+
    return new Level(puzzle, levelPalette);
 };
