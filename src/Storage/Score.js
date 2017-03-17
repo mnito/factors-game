83b991556a65ed3260fe3a5194d6c1313c6b6984
mnito <mpnitowski@gmail.com>
2017-03-17 16:44:30 -0400

    Storage code formatting

--- src/Storage/Score.js
+++ src/Storage/Score.js
@@ -1,55 +1,55 @@
-function Score(results) {
-    this.results = results;
+function Score (results) {
+  this.results = results;
 }
 
-Score.prototype.getResults = function() {
-    //dynamic results fetching
-    if(typeof this.results === 'function') {
-        return this.results();
-    }
-    return this.results;
+Score.prototype.getResults = function () {
+  // Dynamic results fetching
+  if (typeof this.results === 'function') {
+    return this.results();
+  }
+  return this.results;
 };
 
-Score.prototype.totalLevelsPlayed = function() {
+Score.prototype.totalLevelsPlayed = function () {
   return this.getResults().length;
 };
 
-//returns the sum of end numbers from each level
-Score.prototype.compositeFrom = function(firstLevel, lastLevel) {
+// Returns the sum of end numbers from each level
+Score.prototype.compositeFrom = function (firstLevel, lastLevel) {
   var totalLevelsPlayed = this.totalLevelsPlayed();
-  if(firstLevel < 1 || lastLevel < 1) {
-      throw "Level must be greater than 0.";
+  if (firstLevel < 1 || lastLevel < 1) {
+    throw 'Level must be greater than 0.';
   }
-  if(firstLevel > lastLevel) {
-    throw "First level cannot be greater than last level.";
+  if (firstLevel > lastLevel) {
+    throw 'First level cannot be greater than last level.';
   }
-  if(lastLevel > totalLevelsPlayed) {
-      throw "Last level is greater than total levels played.";
+  if (lastLevel > totalLevelsPlayed) {
+    throw 'Last level is greater than total levels played.';
   }
   var composite = 0;
-  for(var i = firstLevel - 1; i < lastLevel; i += 1) {
+  for (var i = firstLevel - 1; i < lastLevel; i += 1) {
     composite += this.getResults()[i].endNumber;
   }
   return composite;
 };
 
-Score.prototype.compositeThrough = function(levelsPlayed) {
+Score.prototype.compositeThrough = function (levelsPlayed) {
   return this.compositeFrom(1, levelsPlayed);
 };
 
-Score.prototype.averageFrom = function(firstLevel, lastLevel) {
+Score.prototype.averageFrom = function (firstLevel, lastLevel) {
   var totalLevels = lastLevel - firstLevel + 1;
   return this.compositeFrom(firstLevel, lastLevel) / totalLevels;
 };
 
-Score.prototype.averageThrough = function(levelsPlayed) {
-    return this.averageFrom(1, levelsPlayed);
+Score.prototype.averageThrough = function (levelsPlayed) {
+  return this.averageFrom(1, levelsPlayed);
 };
 
-Score.prototype.composite = function() {
+Score.prototype.composite = function () {
   return this.compositeThrough(this.totalLevelsPlayed());
 };
 
-Score.prototype.average = function() {
+Score.prototype.average = function () {
   return this.averageThrough(this.totalLevelsPlayed());
 };
--- src/Storage/StorageManager.js
+++ src/Storage/StorageManager.js
@@ -1,42 +1,42 @@
-function StorageManager(storageMethod, prefix) {
-    try {
-        /* With private browsing modes, sometimes localStorage appears to be
-           available yet when attempting to set an item an exception is thrown  */
-        storageMethod.setItem('__available__', true);
-        if(storageMethod.removeItem) {
-            storageMethod.removeItem('__available__');
-        }
-    } catch(e) {
-        throw "Storage method is not currently available.";
+function StorageManager (storageMethod, prefix) {
+  try {
+    /* With private browsing modes, sometimes localStorage appears to be
+       available yet when attempting to set an item an exception is thrown  */
+    storageMethod.setItem('__available__', true);
+    if (storageMethod.removeItem) {
+      storageMethod.removeItem('__available__');
     }
-    this.storageMethod = storageMethod;
-    this.prefix = prefix || '';
+  } catch (e) {
+    throw 'Storage method is not currently available.';
+  }
+  this.storageMethod = storageMethod;
+  this.prefix = prefix || '';
 }
 
-StorageManager.prototype.setCurrentLevel = function(level) {
-    this.storageMethod.setItem(this.prefix + 'level_current', level);
+StorageManager.prototype.setCurrentLevel = function (level) {
+  this.storageMethod.setItem(this.prefix + 'level_current', level);
 };
 
-StorageManager.prototype.incrementCurrentLevel = function() {
-   this.setCurrentLevel(this.getCurrentLevel() + 1);
+StorageManager.prototype.incrementCurrentLevel = function () {
+  this.setCurrentLevel(this.getCurrentLevel() + 1);
 };
 
-StorageManager.prototype.getCurrentLevel = function() {
-    return parseInt(this.storageMethod.getItem(this.prefix + 'level_current')) || 1;
+StorageManager.prototype.getCurrentLevel = function () {
+  return parseInt(this.storageMethod.getItem(this.prefix + 'level_current')) || 1;
 };
 
-StorageManager.prototype.getLevelResults = function() {
-    var results = this.storageMethod.getItem(this.prefix + 'level_results');
-    return results ? JSON.parse(results) : [];
+StorageManager.prototype.getLevelResults = function () {
+  var results = this.storageMethod.getItem(this.prefix + 'level_results');
+  return results ? JSON.parse(results) : [];
 };
 
-StorageManager.prototype.setLevelResult = function(level, result) {
-    var results = this.getLevelResults();
-    //levels are stored in a zero-index array
-    results[level - 1] = result;
-    this.storageMethod.setItem(this.prefix + 'level_results', JSON.stringify(results));
+StorageManager.prototype.setLevelResult = function (level, result) {
+  var results = this.getLevelResults();
+  // Levels are stored in a zero-based array
+  results[level - 1] = result;
+  this.storageMethod.setItem(this.prefix + 'level_results', JSON.stringify(results));
 };
 
-StorageManager.prototype.getLevelResult = function(level) {
-    return this.getLevelResults()[level - 1];
+StorageManager.prototype.getLevelResult = function (level) {
+  return this.getLevelResults()[level - 1];
 };
