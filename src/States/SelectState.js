d94c80077b42a6b1eb182b503efe60a1d86204a5
mnito <mpnitowski@gmail.com>
2017-04-01 13:49:08 -0400

    Reimagine level select view

--- src/States/SelectState.js
+++ src/States/SelectState.js
@@ -1,31 +1,45 @@
 function SelectState (game) {
   this.game = game;
-  var selectView = new LevelSelectView(game.brush, new RenderRegion(0, 0, game.canvas.width, game.canvas.height), game.storageManager, new XSPRNG(1), new MonochromaticPaletteBuilder(), game.config.numberColor, game.canvas.height * 0.7);
+
+  var selectView = new LevelSelectView(game.brush, new RenderRegion(0, 0, game.canvas.width, game.canvas.height), game.storageManager, new XSPRNG(1), new MonochromaticPaletteBuilder(), game.config.numberColor);
   this.selectView = selectView;
-  this.inputMethod = new SliderInput(game.canvas, function (value, x, y, end) {
+
+  var sliderInput = new SliderInput(game.canvas, function (value, x, y, end) {
     selectView.redraw(value, x, y, end);
   });
-  this.inputMethod.onSelect = function (value) {
-    if (value > 0) {
-      game.transition('PLAYING');
-    }
+  this.sliderInput = sliderInput;
+
+  var tryTransition = function(value) {
+      var value = sliderInput.value;
+      if (value > 0 && value <= Math.min(game.score.totalLevelsPlayed() + 1, game.config.levelLimit)) {
+        game.transition('PLAYING');
+      }
   };
+  this.sliderInput.onSelect = tryTransition;
+  this.playButtonInput = new TapInput(game.canvas, [new TapRegion(null, tryTransition)]);
 }
 
 SelectState.prototype.onEnter = function (context) {
   this.game.brush.clearAll();
   this.game.canvas.style.removeProperty('background-color');
-  this.selectView.blockSize = context.levelView.blockSize || context.blockSize;
-  this.inputMethod.min = 0;
-  this.inputMethod.max = Math.min(this.game.score.totalLevelsPlayed() + 1, this.game.config.levelLimit);
-  this.inputMethod.xPad = this.selectView.blockSize / 2;
-  this.inputMethod.yMin = this.selectView.renderRegion.height * 0.7;
-  this.inputMethod.yMax = this.selectView.renderRegion.height * 0.7 + this.selectView.blockSize;
-  this.inputMethod.triggerInitial(context.level.getNumber() || 0);
-  this.inputMethod.listen();
+  this.selectView.blockSize = context.levelView.blockSize;
+  this.selectView.xPad = context.levelView.leftMargin / 2 + (context.levelView.blockSize / 2);
+
+  this.sliderInput.min = 0;
+  this.sliderInput.max = Math.min(this.game.score.totalLevelsPlayed() + 1, this.game.config.levelLimit);
+  this.sliderInput.xPad = this.selectView.xPad;
+  this.sliderInput.yMin = this.selectView.sliderY - this.selectView.blockSize / 2;
+  this.sliderInput.yMax = this.selectView.sliderY + this.selectView.blockSize / 2;
+  this.sliderInput.triggerInitial(context.level.getNumber() || 0);
+  this.sliderInput.listen();
+
+  this.playButtonInput.tapRegions[0].boundingBox = this.selectView.getButtonBoundingBox();
+  this.playButtonInput.listen();
 };
 
 SelectState.prototype.onLeave = function () {
-  this.inputMethod.detach();
-  return { level: this.game.levels.get(this.inputMethod.value) };
+  this.sliderInput.detach();
+  this.playButtonInput.detach();
+
+  return { level: this.game.levels.get(this.sliderInput.value) };
 };
--- src/View/LevelSelectView.js
+++ src/View/LevelSelectView.js
@@ -1,61 +1,87 @@
-function LevelSelectView (brush, renderRegion, storageManager, prng, monochromaticPaletteBuilder, numberColor, blockY, blockSize) {
+function LevelSelectView (brush, renderRegion, storageManager, prng, monochromaticPaletteBuilder, numberColor, blockSize, xPad) {
   this.brush = brush;
   this.renderRegion = renderRegion;
   this.storageManager = storageManager;
   this.prng = prng;
   this.monochromaticPaletteBuilder = monochromaticPaletteBuilder;
   this.numberColor = numberColor;
-  this.blockY = blockY;
+  this.sliderY = this.renderRegion.height * .5
   this.blockSize = blockSize;
+  this.xPad = xPad || 0;
 }
 
-LevelSelectView.prototype.drawNumber = function (value, x, y, end) {
-  this.brush.font = 'bold ' + this.blockSize * 0.5 + 'px sans-serif';
-  this.brush.fillRect(x - this.blockSize / 2, this.blockY, this.blockSize, this.blockSize);
-  this.brush.fillStyle = this.numberColor;
-  if (value < 1) {
-    value = '?';
-  }
-  this.brush.fillText('' + value, x, this.blockY + this.blockSize / 2);
-};
-
 LevelSelectView.prototype.drawLevelResults = function (value) {
-  if (value < 1) {
-    this.drawDefaultView();
-    return;
-  }
   var results = this.storageManager.getLevelResult(value);
   if (!results) {
-    this.drawUnplayedLevelView(value);
     return;
   }
+
   var number = results.endNumber;
   if (number === 1) {
     result = 'ACE!';
   } else {
     result = number < 10 ? 'LOW!' : 'High';
   }
-  result += ' (' + number + ')';
-  this.brush.fillText('Level ' + value, this.renderRegion.width / 2, this.renderRegion.height * 0.10);
-  this.brush.font = 'bold ' + Math.min(this.renderRegion.width * 0.25, 100) + 'px Arial';
-  this.brush.fillText(result, this.renderRegion.width / 2, this.renderRegion.height * 0.30);
-  this.brush.font = Math.min(this.renderRegion.width * 0.1, 40) + 'px Arial';
+
+  this.brush.font = 'bold ' + this.blockSize * 0.5 + 'px sans-serif';
+  this.brush.fillText(result, this.renderRegion.width / 2, this.renderRegion.height * 0.40);
+
+  this.brush.font = 'bold ' + this.blockSize * 0.25 + 'px sans-serif';
+  this.brush.fillText('End Number: ' + number, this.renderRegion.width / 2, this.renderRegion.height * .6);
+  if(number === 1) {
+      this.brush.fillText('Moves: ' + results.path.length, this.renderRegion.width / 2, this.renderRegion.height * .6675);
+  }
 };
 
-LevelSelectView.prototype.drawDefaultView = function () {
-  this.brush.font = 'bold ' + this.blockSize * 0.5 + 'px Arial';
-  this.brush.fillText('Select a Level', this.renderRegion.width / 2, this.renderRegion.height * 0.10);
+LevelSelectView.prototype.drawNumber = function (value, blockColor) {
+  this.brush.fillStyle = blockColor;
+  this.brush.fillRect((this.renderRegion.width / 2) - (this.blockSize / 2), this.renderRegion.height * .15, this.blockSize, this.blockSize);
+
+  if (value < 1) {
+    value = '?';
+  }
+
+  this.brush.font = 'bold ' + this.blockSize * 0.5 + 'px sans-serif';
+  this.brush.fillStyle = this.numberColor;
+  this.brush.fillText('' + value, this.renderRegion.width / 2, (this.renderRegion.height * .15) + (this.blockSize / 2));
 };
 
-LevelSelectView.prototype.drawUnplayedLevelView = function (value) {
-  this.brush.fillText('Level ' + value, this.renderRegion.width / 2, this.renderRegion.height * 0.10);
+LevelSelectView.prototype.drawSlider = function (value, x, y, color) {
+  this.brush.font = 'bold ' + this.blockSize * 0.5 + 'px sans-serif';
+  this.brush.strokeStyle = color;
+  this.brush.lineWidth = 5;
+  this.brush.beginPath();
+
+  this.brush.moveTo(0 + this.xPad / 4, this.sliderY);
+  this.brush.lineTo(this.renderRegion.width - this.xPad / 4, this.sliderY);
+  this.brush.stroke();
+
+  var radius = this.blockSize / 6;
+  this.brush.beginPath();
+  this.brush.arc(x, this.sliderY, radius, 2 * Math.PI, false);
+
+  this.brush.stroke();
+};
+
+LevelSelectView.prototype.drawButton = function (color) {
+  this.brush.fillStyle = color;
+  this.brush.fillRect(this.xPad, this.renderRegion.height * .75, this.renderRegion.width - (this.xPad * 2), this.renderRegion.height * .15);
+  this.brush.font = 'bold ' + this.blockSize * 0.5 + 'px sans-serif';
+  this.brush.fillStyle = this.numberColor;
+  this.brush.fillText('Play', this.renderRegion.width / 2, this.renderRegion.height * .75 + this.renderRegion.height * .075);
+};
+
+LevelSelectView.prototype.getButtonBoundingBox = function() {
+    return new BoundingBox(this.xPad, this.renderRegion.height * .75, this.renderRegion.width - (this.xPad * 2), this.renderRegion.height * .15);
 };
 
 LevelSelectView.prototype.draw = function (value, x, y, end) {
   this.brush.textAlign = 'center';
   this.brush.textBaseline = 'middle';
-  var blockColor = '#000000';
-  if (end) {
+
+  var blockColor = '#808080';
+
+  if (end && value > 0) {
     if (typeof this.prng.seed === 'function') {
       this.prng.seed(value);
     }
@@ -66,10 +92,14 @@ LevelSelectView.prototype.draw = function (value, x, y, end) {
     boardColors = this.monochromaticPaletteBuilder.build(16, 70);
     blockColor = HSL.complement(boardColors[Math.floor(this.prng.random() * 16)]).toString();
   }
-  this.brush.fillStyle = blockColor;
-  this.brush.font = 'bold ' + this.blockSize * 0.5 + 'px Arial';
+
+  this.brush.font = 'bold ' + this.blockSize * 0.25 + 'px sans-serif';
+  this.brush.fillStyle = this.numberColor;
+  this.brush.fillText('LEVEL', this.renderRegion.width / 2, this.renderRegion.height * 0.10);
+  this.drawNumber(value, blockColor);
   this.drawLevelResults(value);
-  this.drawNumber(value, x, y, end);
+  this.drawSlider(value, x, y, this.numberColor);
+  this.drawButton(blockColor);
 };
 
 LevelSelectView.prototype.redraw = function (value, x, y, end) {
